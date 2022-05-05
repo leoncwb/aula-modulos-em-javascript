3610ba336722a2f0035ed790ccc317332ba97663
root <leontecnicalonline@gmail.com>
2022-05-04 21:46:56 -0300

    modificado arquivo README.md, adicionado novos arquivos

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Leonardo', 38));
+console.log(mostraCidade('Leonardo', 'Curitiba'));
+console.log(mostraHobby('Leonardo', 'ler livros'))
